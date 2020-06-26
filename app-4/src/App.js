4e7994b021f9a84cf3891418b20df3de6f877679
azakimi123 <aaron.zakimi@gmail.com>
2020-06-25 21:50:30 -0600

    third practice round

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,27 +1,16 @@
 // import React, {Component} from 'react';  // when using class component and extending
 import React from 'react'; 
-import './App.css';
 import Login from './Components/Login';
+import './App.css';
 
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <Login />
     </div>
   )
 }
 
-//class components use state, if you need to have state then use class
-//this works also
-// class App extends Component {
-//   render() {
 
-//     return (
-//       <div className="App">
-//         <Login />
-//       </div>
-//     )
-//   }
-// }
 export default App;
--- app-4/src/Components/Login.js
+++ app-4/src/Components/Login.js
@@ -2,51 +2,42 @@ import React, {Component} from 'react';
 
 
 class Login extends Component {
-
-
     constructor(props) {
         super(props);
         this.state = {
             username: '',
             password: ''
         }
-        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    handleUsername = (val) => {
+        this.setState({username: val})
+    }
+
+    handlePassword = (pass) => {
+        this.setState({password: pass});
+    }
+
+    handleLogin = () => {
+        alert(`Username: ${this.state.username}   Password: ${this.state.password}`);
     }
 
 
-    handleUsernameChange(name) {
-        this.setState({username: name})
-    }
-
-
-    handlePasswordChange(pass) {
-        this.setState({password: pass})
-    }
-
-    handleLogin() {
-        alert(`You have logged in with ${this.state.username} as your username and ${this.state.password} as your password`);
-    }
-
-
-    render() {
+    render () {
         return (
             <div>
-                <input 
-                    onChange={(e) => this.handleUsernameChange(e.target.value)}
-                    type='text'
+                <input
                     placeholder='username'
-                />
-                <input 
-                    onChange={(e) => this.handlePasswordChange(e.target.value)}
-                    type='text'
+                    value={this.state.username}
+                    onChange={ e => this.handleUsername(e.target.value)}/>
+                <input
                     placeholder='password'
-                />
+                    value={this.state.password}
+                    onChange={ e => this.handlePassword(e.target.value)}/>
                 <button onClick={this.handleLogin}>Login</button>
             </div>
         )
     }
-
 }
-
 
 export default Login;
