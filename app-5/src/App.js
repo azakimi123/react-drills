73ef31af3725bd8506facadb03f9a3231726bae6
azakimi123 <aaron.zakimi@gmail.com>
2020-06-25 21:59:10 -0600

    picture passed as prop

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -2,31 +2,22 @@ import React, {Component} from 'react';
 import './App.css';
 import Image from './Components/Image'
 
-
-
-
-
-class App extends Component{
-
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      image: 'https://picsum.photos/id/1025/367/267'
-    }
-
   }
-  
-  
-  
-  
-  
+
+
   render() {
-  return (
-    <div className="App">
-      <Image picture={this.state.image}/>
-    </div>
-  );
+    return (
+      <div className='App'>
+        <Image picture={'https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68'}/>
+      </div>
+    )
   }
 }
+
+
+
 
 export default App;
--- app-5/src/Components/Image.js
+++ app-5/src/Components/Image.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 
 
-class Image extends Component{
+
+class Image extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
     render() {
-        return(
+        return (
             <div>
-                <img src={this.props.picture} alt = 'cozy pug'/>
+                <img src={this.props.picture}   alt=''/>
             </div>
         )
     }
